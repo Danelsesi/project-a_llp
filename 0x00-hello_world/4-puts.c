cfe64ead2630d1bff2ae412edd5dab174470700f
Joshblueme <josh1234oluwasegun@gmail.com>
2022-10-13 20:14:46 -0700

    Commitment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - puts prints the string "Programming is like building a
- * multilingual puzzle
- * return: main requests an integer which is meant to be void so return
- * prints 0
+ * main - prints the string "Programming is like building a multilingual puzzle
+ * return: 0 if executed, do not return zero otherwise
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
