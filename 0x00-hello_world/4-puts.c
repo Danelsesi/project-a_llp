0e15eb1abf0a412e5df05a114fbce883ebc39aa7
Joshblueme <josh1234oluwasegun@gmail.com>
2022-10-13 20:48:42 -0700

    Commitment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
- #include <stdio.h>
-
- /**
- * main - prints the phrase "Programming is like building a multilingual
- * puzzle
- * return: 0 if executed, do not return zero otherwise
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle\n");
+return (0); }
