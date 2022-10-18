631a206117010417242d86a77d5b3ca7dfadc618
Joshblueme <josh1234oluwasegun@gmail.com>
2022-10-17 20:03:42 -0700

    Commitment

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints the alphabets in lowercase and then in uppercase
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char a_bet = 'a';
+	char A_bet = 'A';
+
+	while (a_bet <= 'z')
+	{
+		putchar(a_bet);
+		a_bet++;
+	} while (A_bet <= 'Z');
+	{
+		putchar(A_bet);
+		A_bet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
